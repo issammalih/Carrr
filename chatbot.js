e952620ca91a87e18e588ef7a3a46489d603cd1c
Dr alami <issammalyh@gmail.com>
2026-05-06 21:43:55 +0100

    Fix worker URL by adding trailing slash

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,4 @@
-const workerUrl = "https://carschabtojdid.issammalyh2.workers.dev";
+const workerUrl = "https://carschatbojdid.issammalyh2.workers.dev/";
 const chatbotHTML = `
 <div class="auto-chatbot-tooltip" id="autoTooltip">
   Can I assist you ? 👋
